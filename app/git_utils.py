7fac2bbfceba66f7486c5983fdb9db1ab37a64aa
Pontus Backman <ponbac@student.chalmers.se>
2021-11-29 23:55:01 +0100

    fixed to analyzing root files

--- app/git_utils.py
+++ app/git_utils.py
@@ -22,22 +22,26 @@ def download_repo(git_url: str):
 # Logic from https://stackoverflow.com/a/9728478
 def list_files(startpath: str):
     full_startpath = startpath
-    if folder_exists(startpath+'/src'):
-        startpath += '/src'
-    elif folder_exists(startpath+'/lib'):
-        startpath += '/lib'
+    # if folder_exists(startpath+'/src'):
+    #     startpath += '/src'
+    # elif folder_exists(startpath+'/lib'):
+    #     startpath += '/lib'
 
     tree_string = ''
     for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        tree_string += f'\n{indent}📂{os.path.basename(root)}/'
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            full_path = os.path.relpath(root).split('/')[2:]
-            path = str('/'.join(full_path)) + f'/{f}'
-            command = f'git log --oneline -- {path} | wc -l'
-            tree_string += f'\n{subindent}📜{f} [{execute_command(full_startpath, command)}]'
+        if '.git' not in os.path.relpath(root):
+            level = root.replace(startpath, '').count(os.sep)
+            indent = ' ' * 4 * (level)
+            tree_string += f'\n{indent}📂{os.path.basename(root)}/'
+            subindent = ' ' * 4 * (level + 1)
+            for f in files:
+                full_path = os.path.relpath(root).split('/')[2:]
+                path = str('/'.join(full_path)) + f'/{f}'
+                # Need this in order to correctly get path to files in root
+                if len(path.split('/')[0]) == 0:
+                    path = path.removeprefix('/')
+                command = f'git log --oneline -- {path} | wc -l'
+                tree_string += f'\n{subindent}📜{f} [{execute_command(full_startpath, command)}]'
 
     return tree_string.strip('\n')
 
@@ -51,7 +55,7 @@ def execute_command(root: str, command: str):
     # Change CWD back to normal one
     os.chdir(pre_cwd)
 
-    #print(f'root: {root} \ncommand: {command} \noutput: {output}')
+    # print(f'root: {root} \ncommand: {command} \noutput: {output}')
     return output.removesuffix('/n').strip()
 
 
